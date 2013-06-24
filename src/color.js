60ffed7119a80587860aee4a95d8dc966986db25
Kiril <vatev.1@gmail.com>
2013-06-24 13:10:52 -0400

    added creators, code cleanup

--- src/color.js
+++ src/color.js
@@ -1,12 +1,49 @@
 (function colorjs(global){
-	console.log(global);
+	var fromName = function(val){
+	
+	};
+	
+	//create Color - HEX to RGBA
+	var fromHEX = function(val){
+		val = String(val).replace(/[^0-9a-f]/gi, "");
+		if (val.length < 6) {
+			val = val[0]+val[0]+val[1]+val[1]+val[2]+val[2];
+		}
+		
+		var toDecimal = function(str){
+			return parseInt(str,16);
+		}
+		
+		var r = toDecimal(val.substring(0,2));
+		var g = toDecimal(val.substring(2,4));
+		var b = toDecimal(val.substring(4,6));
+		
+		return {r:r,g:g,b:b,a:1};
+	}
+	
+	//create Color - RGB/RGBA to valid RGBA
+	var fromRGBA = function(val){
+		var rgba = {};
+		
+		//check for valid color values (0 - 255)
+		// set to 0 if invalid
+		rgba["r"] = (val.r && val.r >= 0 && val.r <= 255) ? val.r : 0;
+		rgba["g"] = (val.g && val.g >= 0 && val.g <= 255) ? val.g : 0;
+		rgba["b"] = (val.b && val.b >= 0 && val.b <= 255) ? val.b : 0;
+		rgba["a"] = (val.a && val.a >= 0 && val.a <= 1) ? val.a : 1;
+		
+		return rgba;
+	}
 	
 	//Color constructor
 	var Color = function(rgba){
 		this.RGBA = rgba;
 	}
 	
-	Color.prototype.HEX = function(){
+	/* converters */
+	Color.prototype.HEX = function(set){
+		if (set) 
+		
 		var hex = "";
 		
 		var pad = function(n){ while(n.length < 2) {n = '0'+n;} return n; }
@@ -27,6 +64,7 @@
 	
 	}
 	
+	/* operations */
 	Color.prototype.lighter = function(percent){
 	
 	}
@@ -35,18 +73,20 @@
 		
 	}
 	
+	/* color schemes */
 	Color.prototype.triad = function(color){
 		return ["color 1", "color 2", "color 3"];
 	}
 	
+	//creator function -- check types
 	var creator = function(val){
 		var rgba = {};
 		
 		if (typeof val === "string") rgba = fromHEX(val);
-		else if (typeof val === "array") rgba = fromArray(val);
 		else if (typeof val === "object") rgba = fromRGBA(val);
 		else rgba = null;
 		
+		//TODO: error check (rgba.r && rgba.g && rgba.b && rgba.a)
 		return new Color(rgba);
 	}
 	
@@ -55,32 +95,20 @@
 		return creator(val);
 	}
 	
-	//create Color from HEX
-	var fromHEX = function(val, color){
-		val = String(val).replace(/[^0-9a-f]/gi, "");
-		if (val.length < 6) {
-			val = val[0]+val[0]+val[1]+val[1]+val[2]+val[2];
-		}
-		
-		var toDecimal = function(str){
-			return parseInt(str,16);
-		}
-		
-		var r = toDecimal(val.substring(0,2));
-		var g = toDecimal(val.substring(2,4));
-		var b = toDecimal(val.substring(4,6));
-		
-		return {r:r,g:g,b:b,a:1};
+	ColorLib.fromHEX = creator;
+	ColorLib.fromRGB = creator;
+	ColorLib.fromRGBA = creator;
+	
+	ColorLib.fromCMYK = function(val){
+		//TODO: CMYK creator
 	}
 	
-	//create Color from RGB or RGBA
-	var fromRGBA = function(val, color){
-		color.RGBA = val;
-		if (!val.a) val.a = 1;
+	ColorLib.fromArray = function(val){
+		return creator({r:val[0], g:val[1], b:val[2], a:val[3]});
 	}
 	
-	ColorLib.prototype.create = function(val){
-		return creator(val);
+	ColorLib.fromRaw = function(r,g,b,a){
+		return creator({r:r, g:g, b:b, a:a});
 	}
 	
 	window.c = ColorLib;
