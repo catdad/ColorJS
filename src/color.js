66b9e00bfef07fbaf21d3b8f5345bc985c16b7d4
Kiril <vatev.1@gmail.com>
2013-06-27 01:43:27 -0400

    cleanup

--- src/color.js
+++ src/color.js
@@ -3,38 +3,6 @@
 	
 	};
 	
-	//create Color - HEX to RGBA
-	var fromHEX = function(val){
-		val = String(val).replace(/[^0-9a-f]/gi, "");
-		if (val.length < 6) {
-			val = val[0]+val[0]+val[1]+val[1]+val[2]+val[2];
-		}
-		
-		var toDecimal = function(str){
-			return parseInt(str,16);
-		}
-		
-		var r = toDecimal(val.substring(0,2));
-		var g = toDecimal(val.substring(2,4));
-		var b = toDecimal(val.substring(4,6));
-		
-		return {r:r,g:g,b:b,a:1};
-	}
-	
-	//create Color - RGB/RGBA to valid RGBA
-	var fromRGBA = function(val){
-		var rgba = {};
-		
-		//check for valid color values (0 - 255)
-		// set to 0 if invalid
-		rgba["r"] = (val.r && val.r >= 0 && val.r <= 255) ? val.r : 0;
-		rgba["g"] = (val.g && val.g >= 0 && val.g <= 255) ? val.g : 0;
-		rgba["b"] = (val.b && val.b >= 0 && val.b <= 255) ? val.b : 0;
-		rgba["a"] = (val.a && val.a >= 0 && val.a <= 1) ? val.a : 1;
-		
-		return rgba;
-	}
-	
 	//Color constructor
 	var Color = function(rgba){
 		this.RGBA = rgba;
@@ -304,26 +272,49 @@
 	
 	//creator function -- check types
 	var creator = function(val){
+		//check for valid color values (0 - 255)
+		// set to 0 if invalid
 		var rgba = {};
+		rgba["r"] = (val.r && val.r >= 0 && val.r <= 255) ? val.r : 0;
+		rgba["g"] = (val.g && val.g >= 0 && val.g <= 255) ? val.g : 0;
+		rgba["b"] = (val.b && val.b >= 0 && val.b <= 255) ? val.b : 0;
+		rgba["a"] = (val.a && val.a >= 0 && val.a <= 1) ? val.a : 1;
 		
-		if (typeof val === "string") rgba = fromHEX(val);
-		else if (typeof val === "object") rgba = fromRGBA(val);
-		else rgba = null;
-		
-		//TODO: error check (rgba.r && rgba.g && rgba.b && rgba.a)
 		return new Color(rgba);
 	}
 	
 	//simple creator -- RGB[A] or HEX
 	var ColorLib = function(val){
-		return creator(val);
+		//checl for null
+		if (!val) return null;
+		
+		if (typeof val === "string") return ColorLib.fromHEX(val);
+		else return creator(val);
 	}
 	
-	ColorLib.fromHEX = creator;
-	ColorLib.fromRGB = creator;
-	ColorLib.fromRGBA = creator;
+	//xxxxxx, xxx, #xxx, or #xxxxxx
+	ColorLib.fromHEX = function(val){
+		val = String(val).replace(/[^0-9a-f]/gi, "");
+		if (val.length < 6) {
+			val = val[0]+val[0]+val[1]+val[1]+val[2]+val[2];
+		}
+		
+		var toDecimal = function(str){
+			return parseInt(str,16);
+		}
+		
+		var r = toDecimal(val.substring(0,2));
+		var g = toDecimal(val.substring(2,4));
+		var b = toDecimal(val.substring(4,6));
+		
+		return creator({r:r,g:g,b:b,a:1});
+	}
 	
-	// [c,m,y,k] or {c,m,y,k}
+	//create from RGB or RGBA objects
+	ColorLib.fromRGBA = creator;
+	ColorLib.fromRGB = creator;
+	
+	// RGB from [c,m,y,k] or {c,m,y,k}
 	ColorLib.fromCMYK = function(val){
 		var CMYK = {};
 		if (val instanceof Array){
